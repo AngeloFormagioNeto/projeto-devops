3364bc964314088419ac90d45cbbd1fac3d36a77
AngeloFormagioNeto <angelo.formagio00@edu.pucrs.br>
2025-06-04 19:47:26 -0300

    adicionando teste para ItemList

--- src/components/ItemList/ItemList.test.js
+++ src/components/ItemList/ItemList.test.js
@@ -1,8 +1,8 @@
-import React from 'react'; // Importe React se usar JSX
+import React from 'react';
 import renderer from 'react-test-renderer';
-import ItemList from './ItemList';
+import ItemList from './index';
 
-test('ItemList snapshot', () => {
+test('ItemList snapshot matches', () => {
   const tree = renderer
     .create(<ItemList title="Test Repo" description="Test description" />)
     .toJSON();
