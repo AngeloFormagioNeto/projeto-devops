ae8108a148003a7bd51e3e56c0c7b279e7bd0b50
AngeloFormagioNeto <angelo.formagio00@edu.pucrs.br>
2025-06-18 13:45:25 -0300

    teste

--- terraform/main.tf
+++ terraform/main.tf
@@ -278,19 +278,3 @@ resource "aws_cloudwatch_log_group" "ecs_logs" {
     Name = "logs-${random_string.suffix.result}"
   }
 }
-
-# Outputs
-output "alb_dns_name" {
-  value       = aws_lb.app.dns_name
-  description = "DNS do Application Load Balancer"
-}
-
-output "ecs_cluster_name" {
-  value       = aws_ecs_cluster.main.name
-  description = "Nome do cluster ECS"
-}
-
-output "ecs_service_name" {
-  value       = aws_ecs_service.app.name
-  description = "Nome do serviço ECS"
-}
